390981442c6e5c1d2da54b6c17f73ca3c775f9f6
JakeWard122 <jward466@gmail.com>
2017-04-21 19:46:56 +0100

    Add the button to add the word created to the end text box.

--- Form1.cs
+++ Form1.cs
@@ -13,28 +13,28 @@ namespace minikeyboard
 
     public partial class Form1 : Form
     {
-         string word = "";
-            int button1Clicks = 0;
-            int button2Clicks = 0;
-            int button3Clicks = 0;
-            int button4Clicks = 0;
-            int button5Clicks = 0;
-            int button6Clicks = 0;
-            int button7Clicks = 0;
-            int button8Clicks = 0;
+        string word = "";
+        int button1Clicks = 0;
+        int button2Clicks = 0;
+        int button3Clicks = 0;
+        int button4Clicks = 0;
+        int button5Clicks = 0;
+        int button6Clicks = 0;
+        int button7Clicks = 0;
+        int button8Clicks = 0;
 
 
         public Form1()
         {
             InitializeComponent();
-           
+
         }
 
         private void btn_mode_Click(object sender, EventArgs e)
         {
             tbx_Mode.Text = "Prediction";
         }
-        
+
         private void btn_1_Click(object sender, EventArgs e)
         {
             button1Clicks++;
@@ -129,14 +129,14 @@ namespace minikeyboard
 
         private void tbx_WordBuild_TextChanged(object sender, EventArgs e)
         {
-           
+
         }
 
         public void Wordbuilder()
         {
             tbx_WordBuild.Text += word;
-        
-        
+
+
         }
 
         private void button1Timer_Tick(object sender, EventArgs e)
@@ -148,10 +148,15 @@ namespace minikeyboard
 
         private void btn_000_Click(object sender, EventArgs e)
         {
-            
+            Tbx_Words.Text += tbx_WordBuild.Text;
+            tbx_WordBuild.Text = "";
+
+            button1Timer.Enabled = false;
+            button1Clicks = 0;
+            word = "";
         }
 
 
-        
+
     }
 }
